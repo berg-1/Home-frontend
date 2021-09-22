3e63cd2433cb9937367d18da47622569186f5f4c
berg-1 <884441972@qq.com>
2021-09-22 23:54:24 +0800

    Add horizontal multi-item gallery and button control

--- static/js/init.js
+++ static/js/init.js
@@ -55,10 +55,95 @@ function togglePrismTheme() {
     }
 }
 
+function toc() {
+    $(function () {
+        const navSelector = "#table_of_content";
+        const $myNav = $(navSelector);
+        Toc.init($myNav);
+        $("#article").scrollspy({
+            target: navSelector
+        });
+    });
+}
+
 const themeSwitch = document.querySelector('input');
 themeSwitch.addEventListener('change', () => {
     document.body.classList.toggle('dark-theme');  // change web mode
     togglePrismTheme()  // switch prism theme
 });
 
+const renderer = new marked.Renderer();
+renderer.code = function (code, lang, escaped) {
+    code = this.options.highlight(code, lang);
+    if (!lang) {
+        return `<pre><code class="language-text">${code}</code></pre>`;
+    }
+    const langClass = "language-" + lang;
+    return `<pre class="${langClass} rainbow-braces line-numbers"><code class="${langClass} match-braces">${code}</code></pre>`;
+}
 
+marked.setOptions({
+    renderer: renderer,
+    highlight: function (code, lang) {
+        try {
+            console.log("Prism initialization")
+            return Prism.highlight(code, Prism.languages[lang], lang);
+        } catch {
+            return code;
+        }
+    },
+});
+
+
+// horizontal gallery
+let scrollWidth = 1000
+const scroll = $("#scroll")[0];
+const prev = $("#scroll_prev");
+const next = $("#scroll_next");
+const maxScrollLeft = scroll.scrollWidth - scroll.clientWidth;
+scroll.onscroll = function () {
+    if (scroll.scrollLeft === 0) {
+        prev.addClass('disabled')
+    } else if (scroll.scrollLeft === maxScrollLeft) {
+        next.addClass('disabled')
+    } else {
+        prev.removeClass('disabled')
+        next.removeClass('disabled')
+    }
+}
+
+prev[0].onclick = function () {
+    scroll.scrollBy({
+        top: 0,
+        left: -scrollWidth,
+        behavior: 'smooth'
+    });
+};
+
+next[0].onclick = function () {
+    scroll.scrollBy({
+        top: 0,
+        left: scrollWidth,
+        behavior: 'smooth'
+    });
+};
+
+// $.ajax({
+//     "type": 'get',
+//     "url": 'http://localhost:8080/downloadFile?id=1134629f-c718-4a7f-966e-c0483adf0d12',
+//     "dataType": "text",
+//     "permissions": "http://localhost:8080/",
+//     "success": function (data) {
+//         document.getElementById('content').innerHTML = marked(data);
+//         console.log("ajax:" + new Date())
+//         Prism.highlightAll();  // Rerun Prism syntax highlighting on the current page
+//     }
+// })
+
+toc()
+$(document).ready(function () {
+    // Handler for .ready() called.
+    console.log("document is ready!")
+    prev.addClass('disabled')  // init status
+    $('ul.navbar-nav .active').removeClass('active');
+});
